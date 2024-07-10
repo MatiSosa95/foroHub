9e96424cf862f558c0babf925bdf8f0399e35451
Maty <mati.sosa95@hotmail.com>
2024-07-10 11:44:41 -0300

    v2.1 swagger add

--- src/main/java/com/Alura/ForoHub/Infra/Seguridad/SecurityFilter.java
+++ src/main/java/com/Alura/ForoHub/Infra/Seguridad/SecurityFilter.java
@@ -25,14 +25,13 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = request.getHeader("Authorization");
-        System.out.println(token);
         if(token!=null){
-            token= token.replace("Bearer", "");
+            token= token.replace("Bearer ", "");
             var subject= tokenService.getSubject(token);
             if (subject!=null){
                 var usuario= autorRepository.findByEmail(subject);
                 var authentication= new UsernamePasswordAuthenticationToken(usuario, null,
-                        usuario.getAuthorities());
+                        usuario.getAuthorities());//Se fuerza el inicio de sesion
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
